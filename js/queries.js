ac33daa18b11bed5250b615df5274ac8d2eed07f
mendes <ildefonsa.gregoriomendes@unifr.ch>
2016-05-03 11:46:01 +0200

    Update queries.js

--- js/queries.js
+++ js/queries.js
@@ -6,21 +6,43 @@ PREFIX dc: <http://purl.org/dc/elements/1.1/>
 PREFIX mo: <http://purl.org/ontology/mo/>
 PREFIX event: <http://purl.org/NET/c4dm/event.owl#> 
 PREFIX lkb: <http://linkedbrainz.org/>
+PREFIX dbpedia: <http://dbpedia.org/resource/>
+PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
 `;
 var q1 = prefixes +`
 construct {
   lkb:{artist} a mo:MusicArtist;
       lkb:ArtistType ?type;
       foaf:isPrimaryTopicOf ?url;
-      foaf:gender ?gender.
+      foaf:gender ?gender;
+      dbpedia-owl:abstract ?abstract ;
+      dbpedia-owl:thumbnail ?thumbnail;
+      lkb:members ?members.
+     
+  ?members a lkb:members;
+    foaf:name ?name;
+    foaf:gender ?m_gender.
 }
 where {
   ?artist_gid foaf:name '{artist}';
-  rdf:type ?type.
-    ?artist_gid foaf:isPrimaryTopicOf ?url.
-    OPTIONAL {?artist_gid foaf:gender ?gender .}.
-    FILTER (?type != <http://purl.org/ontology/mo/MusicArtist>)
-}`;
+  foaf:isPrimaryTopicOf ?url.
+  ?artist_gid   rdf:type ?type.
+  ?artist_gid foaf:isPrimaryTopicOf ?url.
+  OPTIONAL {?artist_gid foaf:gender ?gender .}.
+  SERVICE <http://dbpedia.org/sparql> {
+         ?dbpedia_url foaf:isPrimaryTopicOf  ?url;
+         dbpedia-owl:abstract ?abstract ;
+        dbpedia-owl:thumbnail ?thumbnail .
+
+   FILTER (langMatches(lang(?abstract),"fr"))
+  }
+  ?members mo:member_of ?artist_gid;
+  foaf:name ?name;
+  foaf:gender ?m_gender.
+
+ FILTER (?type != <http://purl.org/ontology/mo/MusicArtist>)
+}
+`;
 
 var q2 = prefixes+`
 construct {
@@ -30,6 +52,7 @@ construct {
  ?release a lkb:release;
     dc:title ?title;
     dc:date ?date;
+    lkb:link ?release;
     event:place ?placeTitle.
 }
 where {
@@ -40,6 +63,25 @@ where {
   dc:date ?date;
   event:place ?place.
   ?place rdfs:label ?placeTitle.
-}
+} ORDER BY DESC(?date)
 `;
+var q3= prefixes+`
+construct {
+  lkb:Manowar a mo:MusicArtist;
+    lkb:track ?track.
 
+  ?track a lkb:track;
+    dc:title ?_title;
+    mo:duration ?_duration.
+}
+where {
+<{release}> dc:title ?r_title;
+mo:record ?record.
+?record mo:track ?track.
+?track dc:title ?_title;
+ mo:duration ?_duration.
+
+bind( xsd:integer(?_duration) as ?duration )
+bind( str(?_title) as ?title )
+} 
+`;
